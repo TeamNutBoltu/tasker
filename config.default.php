4be65809f543a2acbdd1c5002d0ccc0e2da63593
Team NutBoltu HackZone <hack@nutboltu.net>
2015-05-11 17:17:49 +0600

    config.default.php file added.

---
+++ config.default.php
@@ -0,0 +1,13 @@
+<?php
+
+// All the database configuration data will go here.
+
+define('HOST', 'Your host name'); // Your database host name or ip of the database.
+
+define('USERNAME', 'Your username name'); // Username of your database.
+
+define('PASSWORD', 'Your database password'); // Password of your database user.
+
+define('DATABASE', 'Your database name'); // Name of your database.
+
+?>
